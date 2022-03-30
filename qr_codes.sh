9d724dc1ee40535d382fe1fd0950acfd5b8e196e
Fernando Pérez-García <fepegar@gmail.com>
2022-03-30 13:48:43 +0100

    Add QR codes

---
+++ qr_codes.sh
@@ -0,0 +1,6 @@
+pip install --quiet "qrcode[pil]"
+qr "https://bit.ly/luos-obj-plan" > luos_objection_planning.png
+qr "https://bit.ly/luos-comments" > example_comments.png
+qr "mailto:Philip.Elliott@haringey.gov.uk" > philip_elliott_email.png
+qr "https://bit.ly/luos-obj-pdf" > luos_objection_pdf.png
+qr "https://bit.ly/luos-appl-details" > application_details.png
